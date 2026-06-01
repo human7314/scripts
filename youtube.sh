68481fb865615a2ab0827a52cdd9f9329a012086
Omkar <8.omkaryadav@gmail.com>
2026-06-01 13:37:42 +0530

    Edited yt script

--- youtube.sh
+++ youtube.sh
@@ -1,23 +1,7 @@
-#/bin/sh
-
-# Check clipboard first
-# url=$(xclip -o -selection clipboard)
-# 
-# if echo "$url" | grep -qE '^https?://'; then
-#     mpv "$url"
-# else
-#     url=$(echo "" | dmenu -p "YouTube URL:")
-#     [ -z "$url" ] && exit
-#     mpv "$url"
-# fi
-
 #!/bin/sh
-
-# Prompt for a YouTube URL using dmenu
-url=$(echo "" | dmenu)
-
-# Exit if nothing entered
-[ -z "$url" ] && exit
-
-# Play video using mpv
+url=$(xclip -o -selection clipboard 2>/dev/null)
+echo "$url" | grep -qE '^https?://' || {
+    url=$(dmenu -p "URL:" < /dev/null)
+    [ -z "$url" ] && exit
+}
 mpv "$url"
